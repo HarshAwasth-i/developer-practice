531c6692aeadf19dbc2e453280e818b9073c3be9
Harsh <harshawasthi2023@gmail.com>
2026-08-21 23:41:35 +0530

    Implemented tasks

--- Day-5-Secure-Task-Manager/backend/models/Task.js
+++ Day-5-Secure-Task-Manager/backend/models/Task.js
@@ -1,82 +1,51 @@
 const mongoose = require("mongoose");
 
 
-const taskSchema = new mongoose.Schema(
+const taskSchema = new mongoose.Schema({
 
-    {
-
-        title: {
-
-            type: String,
-
-            required: true,
-
-            trim: true,
-
-        },
-
-
-        description: {
-
-            type: String,
-
-            default: "",
-
-        },
-
-
-        priority: {
-
-            type: String,
-
-            enum: ["Low", "Medium", "High"],
-
-            default: "Medium"
-
-        },
-
-
-        completed: {
-
-            type: Boolean,
-
-            default: false,
-
-        },
-
-
-        // Project this task belongs to
-
-        project: {
-
-            type: mongoose.Schema.Types.ObjectId,
-
-            ref: "Project",
-
-            default: null,
-
-        },
-
-
-        user: {
-
-            type: mongoose.Schema.Types.ObjectId,
-
-            ref: "User",
-
-            required: true,
-
-        },
-
+    title:{
+        type:String,
+        required:true,
+        trim:true
     },
 
-    {
 
-        timestamps: true,
+    description:{
+        type:String,
+        default:""
+    },
 
+
+    priority:{
+        type:String,
+        enum:["Low","Medium","High"],
+        default:"Medium"
+    },
+
+
+    completed:{
+        type:Boolean,
+        default:false
+    },
+
+
+    project:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Project",
+        default:null
+    },
+
+
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
     }
 
-);
+
+},{
+    timestamps:true
+});
 
 
-module.exports = mongoose.model("Task", taskSchema);
+module.exports = mongoose.model("Task",taskSchema);
