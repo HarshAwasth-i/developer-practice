aeec5912f42b0435b1634e92a96454aeea28068e
Harsh <harshawasthi2023@gmail.com>
2026-08-27 23:38:50 +0530

    Kanban Card improved

--- Day-5-Secure-Task-Manager/frontend/src/components/KanbanCard.js
+++ Day-5-Secure-Task-Manager/frontend/src/components/KanbanCard.js
@@ -50,7 +50,7 @@ function KanbanBoard({
 
                 tasks={inProgressTasks}
 
-                onStatusChange={onStatusChange}
+                onStatusChange = {onStatusChange}
 
             />
 
