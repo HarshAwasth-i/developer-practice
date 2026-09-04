6e5770f4865170264ab5523b2239135d5c282890
Harsh <harshawasthi2023@gmail.com>
2026-09-05 00:17:42 +0530

    Prepare TaskPulse for deployment

--- Day-5-Secure-Task-Manager/frontend/src/api/axios.js
+++ Day-5-Secure-Task-Manager/frontend/src/api/axios.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
+    return config;
 });
 
 export default API;
